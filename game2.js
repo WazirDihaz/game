b3dd441a23110e3854afa4a2b0afc8f72f934d9b
WazirDihaz <166924512+WazirDihaz@users.noreply.github.com>
2024-09-23 23:53:37 -0700

    game

---
+++ game2.js
@@ -0,0 +1,18 @@
+console.log("i am still here");
+
+
+const obj=
+{
+idd:4,
+name:"zahid",
+classs:"software",
+location:"mirpur",
+city:"gilgit",
+functionmy:()=>
+{
+    console.log(" id "+this.idd+" class :" +this.classs+"locarin"+this.location);
+}
+}
+console.log(obj.functionmy);
+ 
+
